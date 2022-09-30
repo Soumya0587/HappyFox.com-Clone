1b018e4f56c834cac5259d3b80a5414f1795f288
Soumya0587 <ghoshalsoumalya96@gmail.com>
2022-10-01 00:53:22 +0530

    update

--- login.js
+++ login.js
@@ -1,15 +1,16 @@
 document.querySelector("form").addEventListener("submit",mysignin)
-
+let dataset= JSON.parse(localStorage.getItem("details")) || []
 function mysignin(event){
     event.preventDefault()
     let email=document.querySelector("#email").value
     let password=document.querySelector("#password").value
 
-   let dataset= JSON.parse(localStorage.getItem("details"))
+   
 
    for(let i=0;i<dataset.length;i++){
     if(email==dataset[i].email && password==dataset[i].password ){
         alert("logged in")
+        
         // window.open("body.html")
     }else if(email !==dataset[i].email){
         alert("Incorrect email")
